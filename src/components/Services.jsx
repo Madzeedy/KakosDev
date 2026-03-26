de9d865d28d213133168aee7b139071cc530ae21
Madjid <harunamadjid226@gmail.com>
2026-03-26 18:29:27 +0200

    Designing the Servvices component

---
+++ src/components/Services.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Services() {
+  return (
+    <section className="services" id="services">
+      <h2>Our Services</h2>
+    </section>
+  );
+}
+
+export default Services;
